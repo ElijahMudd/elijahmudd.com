8b44dc5f0d0b246ab0a4f31ebcf8c9de1f8d34f1
Elijah <167829059+ElijahMuddy@users.noreply.github.com>
2026-07-21 13:31:21 -0400

    Add files via upload

---
+++ Spinning Spells/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Elijah Mudd-Spinning Spells-1.0";
+const contentToCache = [
+    "Build/Spinning Spells.loader.js",
+    "Build/Spinning Spells.framework.js.gz",
+    "Build/Spinning Spells.data.gz",
+    "Build/Spinning Spells.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
